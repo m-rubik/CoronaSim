06c2d4e9c7b9054ed57de2c66062e0d7d7ba8d36
Lucas Nieuwenhout <lucasnwnht@gmail.com>
2020-03-22 17:15:55 -0600

    Initial Commit

--- CoronaSim/main.cpp
+++ CoronaSim/main.cpp
@@ -8,8 +8,51 @@
 
 #include <iostream>
 
+class person {
+public:
+    // Is the person infected
+    std::string status = "Healthy";
+    // Has the person been infected and then healed
+    
+    
+    // Unit time that the person has been infected
+    int sicktime;
+    
+    // Position of the person
+    int position[2];
+    
+    // Which group is the person in
+    int group;
+    
+    
+    void infect() {
+        status = "Sick";
+    };
+    
+    void heal() {
+        status = "Healed";
+    }
+    
+    
+    
+    
+};
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    person Lucas;
+    
+    std::cout << Lucas.status << "\n";
+    
+    Lucas.infect();
+    
+    std::cout << Lucas.status << "\n";
+    
+    
+    
+    
     return 0;
 }
