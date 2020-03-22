c4b7d3c70bb280c65b1cfb4b519bc83ecd1980c0
Lucas Nieuwenhout <lucasnwnht@gmail.com>
2020-03-22 16:16:27 -0600

    Initial Commit

---
+++ CoronaSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CoronaSim
+//
+//  Created by Lucas Nieuwenhout on 2020-03-22.
+//  Copyright © 2020 Lucas Nieuwenhout. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
